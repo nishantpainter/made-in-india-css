485586ae790cdd95c3199bfe84d173e82f421cba
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-08-08 07:38:21 +0530

    Add back to top button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 100) {
+!(function ($) {
+  "use strict";
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#navigator").addClass("navigator-scrolled");
+    } else {
+      $("#navigator").removeClass("navigator-scrolled");
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
     $("#navigator").addClass("navigator-scrolled");
-  } else {
-    $("#navigator").removeClass("navigator-scrolled");
   }
-});
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+
+  $(".back-to-top").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      "easeInOutExpo"
+    );
+    return false;
+  });
+})(jQuery);
